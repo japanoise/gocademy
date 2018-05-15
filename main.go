f5cef08d403f560e66b9d62c04d3aab030944c87
japanoise <japanoise@users.noreply.github.com>
2018-05-15 10:00:38 +0100

    Create package for map functions

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
-import "github.com/nsf/termbox-go"
+import (
+	"github.com/japanoise/gocademy/maps"
+	"github.com/nsf/termbox-go"
+)
 
 func main() {
 	termbox.Init()
 	defer termbox.Close()
 
-	DrawTile(0x0c2e, 0, 0)
-	DrawTile(0x0dac, 0, 1)
-	DrawTile(0x0023, 0, 2)
-	DrawTile(0xc02b, 0, 3)
-	DrawTile(0xcc2b, 0, 4)
+	maps.DrawTile(0x0c2e, 0, 0)
+	maps.DrawTile(0x0dac, 0, 1)
+	maps.DrawTile(0x0023, 0, 2)
+	maps.DrawTile(0xc02b, 0, 3)
+	maps.DrawTile(0xcc2b, 0, 4)
 	termbox.Flush()
 
 	termbox.PollEvent()
--- maps/tile.go
+++ maps/tile.go
@@ -1,4 +1,4 @@
-package main
+package maps
 
 import "github.com/nsf/termbox-go"
 
